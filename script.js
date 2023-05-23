4b532eb1d602d5fd45dca982a317e6d28f7a1233
Luis <luisfelipe.tecnologias@gmail.com>
2023-05-23 13:54:27 -0300

    testes

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
+async function buscaEndereco() {
     try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
-
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
-
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
-        console.log(erro);
+        console.log(consultaCEPConvertida)
+    } catch (erro){
+        console.log(erro)
     }
 }
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+buscaEndereco();
+
