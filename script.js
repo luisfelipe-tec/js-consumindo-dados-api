290210d9c7d0b06d42aacd75bc2863bfe3ed1ef7
Luis <luisfelipe.tecnologias@gmail.com>
2023-05-23 15:42:53 -0300

    preenchimento automatico das informações endereço

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML="";
     try {
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
-        console.log(consultaCEPConvertida)
+        var cidade = document.getElementById('cidade')
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+        
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
     } catch (erro){
+        mensagemErro.innerHTML = `<p> CEP inválido. Tente novamente!</p>`
         console.log(erro)
     }
 }
 
 
-buscaEndereco();
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
